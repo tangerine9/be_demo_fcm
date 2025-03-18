5d7327e49758841b4c98d1d8875fffd5edb56397
smkang <smkang@ontacthealth.com>
2025-03-18 10:15:05 +0900

    inti

---
+++ src/main/kotlin/com/tangerine9/demofcm/DemoFcmApplication.kt
@@ -0,0 +1,11 @@
+package com.tangerine9.demofcm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoFcmApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoFcmApplication>(*args)
+}
